8549ebbd63334fd24a6bf1b2bf739659a4b7a033
Aarush1k <76858126+Aarush1k@users.noreply.github.com>
2021-03-30 20:07:54 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,31 @@
+import discord
+import asyncio
+import requests
+import os
+from discord.ext import commands
 
+from PIL import Image
+from io import BytesIO
+
+@client.event
+async def on_ready():
+    await client.change_presence(status=discord.Status.idle, activity=discord.Game('A.help'))
+    print('Bot is ready you dum dum.')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Your Ping is {round(client.latency * 1000)}ms')
+
+
+@client.command()
+async def nuke(ctx, amount = 900):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send("Channel Nuked!")
